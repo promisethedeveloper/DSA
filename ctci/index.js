c28fa930e2123d583e1612cf93f95ce69a937243
promisethedeveloper <onyekamorka06@gmail.com>
2025-07-17 19:26:40 -0500

    Sorted square

--- ctci/index.js
+++ ctci/index.js
@@ -2,43 +2,122 @@ function isUnique(str) {
   let hashMap = new Map();
   for (let i = 0; i < str.length; i++) {
     if (hashMap.has(str[i])) {
-      return true;
+      return false;
     } else {
       hashMap.set(str[i], i);
     }
   }
-  return false;
+
+  return true;
 }
 
-function checkPermutation(str1, str2) {
-  if (str1.length !== str2.length) {
-    return false;
-  }
-  let hashMap = new Map();
-  for (let i = 0; i < str1.length; i++) {
-    if (hashMap.has(str1[i])) {
-      hashMap.set(str1[i], hashMap.get(str1[i]) + 1);
-    } else {
-      hashMap.set(str1[i], 1);
-    }
-  }
-  for (let i = 0; i < str2.length; i++) {
-    if (hashMap.has(str2[i])) {
-      hashMap.set(str2[i], hashMap.get(str2[i]) - 1);
-      if (hashMap.get(str2[i]) === 0) {
-        hashMap.delete(str2[i]);
-      }
-    } else {
+function checkPalindrome(s) {
+  let leftPointer = 0;
+  let rightPointer = s.length - 1;
+
+  while (leftPointer < rightPointer) {
+    if (s[leftPointer] !== s[rightPointer]) {
       return false;
+    } else {
+      leftPointer++;
+      rightPointer--;
     }
   }
   return true;
 }
 
-function sortString(str) {
-  return str.split("").sort().join("");
+function checkForTarget(arr, t) {
+  let leftPointer = 0;
+  let rightPointer = arr.length - 1;
+  while (leftPointer < rightPointer) {
+    let currSum = arr[leftPointer] + arr[rightPointer];
+    if (currSum === t) {
+      return true;
+    } else if (currSum > t) {
+      rightPointer--;
+    } else {
+      leftPointer++;
+    }
+  }
+  return false;
 }
 
-function checkPermutation2(str1, str2) {
-  return sortString(str1) === sortString(str2);
+function combine(arr1, arr2) {
+  let arrOnePointer = 0;
+  let arrTwoPointer = 0;
+  let answerArray = [];
+
+  while (arrOnePointer < arr1.length && arrTwoPointer < arr2.length) {
+    let arrOneElem = arr1[arrOnePointer];
+    let arrTwoElem = arr2[arrTwoPointer];
+
+    if (arrOneElem < arrTwoElem) {
+      answerArray.push(arrOneElem);
+      arrOnePointer++;
+    } else if (arrTwoElem < arrOneElem) {
+      answerArray.push(arrTwoElem);
+      arrTwoPointer++;
+    } else {
+      answerArray.push(arrOneElem);
+      arrOnePointer++;
+    }
+  }
+
+  while (arrOnePointer < arr1.length) {
+    answerArray.push(arr1[arrOnePointer]);
+    arrOnePointer++;
+  }
+  while (arrTwoPointer < arr2.length) {
+    answerArray.push(arr2[arrTwoPointer]);
+    arrTwoPointer++;
+  }
+  return answerArray;
+}
+
+function isSequence(s, t) {
+  let sIndex = 0;
+  let tIndex = 0;
+
+  while (sIndex < s.length && tIndex < t.length) {
+    if (s[sIndex] === t[tIndex]) {
+      sIndex++;
+    }
+    tIndex++;
+  }
+
+  return sIndex === s.length;
+}
+
+var reverseString = function (s) {
+  let leftPointer = 0;
+  let rightPointer = s.length - 1;
+  while (leftPointer < rightPointer) {
+    let holdValue = s[leftPointer];
+    s[leftPointer] = s[rightPointer];
+    s[rightPointer] = holdValue;
+    leftPointer++;
+    rightPointer--;
+  }
+};
+
+function sortedSquare(nums) {
+  let leftIndex = 0;
+  let rightIndex = nums.length - 1;
+  let lastIndex = nums.length - 1;
+  let answerArr = [];
+
+  while (leftIndex <= rightIndex) {
+    let leftValue = nums[leftIndex] * nums[leftIndex];
+    let rightValue = nums[rightIndex] * nums[rightIndex];
+
+    if (leftValue > rightValue) {
+      answerArr[lastIndex] = leftValue;
+      leftIndex++;
+    } else {
+      answerArr[lastIndex] = rightValue;
+      rightIndex--;
+    }
+    lastIndex--;
+  }
+  return answerArr;
 }
